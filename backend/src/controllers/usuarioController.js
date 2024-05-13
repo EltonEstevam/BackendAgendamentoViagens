9bc48147bb0c05e2638be13e1ac8c4926daf04d2
EltonEstevam <eltonestevam@live.com>
2024-05-13 13:57:22 -0300

    Modificado o servidor para utlizar SSL, e feita a parte para validar o login do usuario ao sistema.

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -10,18 +10,20 @@ const getID = async (request, response) => {
 };
 
 //------------------------------------------------------------------
-
+// Seleciona todos os usuarios cadastrados:
 const getAllUsuarios = async (_request, response) => {
   const usuarios = await usuarioModel.getAllUsuarios();
   return response.status(200).json(usuarios);
 };
 
 //------------------------------------------------------------------
+// Cadastrar um novo usuario:
 const createUsuario = async (request, response) => {
   const createdUsuario = await usuarioModel.createUsuario(request.body);
   return response.status(201).json(createdUsuario);
 };
 //------------------------------------------------------------------
+// Deletar um usuario:
 const deleteUsuario = async (request, response) => {
   const { id } = request.params;
 
@@ -29,6 +31,7 @@ const deleteUsuario = async (request, response) => {
   return response.status(204).json();
 };
 //------------------------------------------------------------------
+// Atualizar um usuario:
 const updateUsuario = async (request, response) => {
   const { id } = request.params;
 
@@ -36,10 +39,37 @@ const updateUsuario = async (request, response) => {
   return response.status(204).json();
 };
 //------------------------------------------------------------------
+// Validar usurio com base em email e senha:
+const validaUsuario = async (request, response) => {
+  const Credencial = await usuarioModel.validarUsuario(request.body);
+
+  // Função para verificar se o ID existe:
+  function verificaUsuario(Credencial) {
+    const id = Credencial[0];
+    //console.log(id);
+    if (id) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  let res = verificaUsuario(Credencial);
+  if (res == false) {
+    return response.status(404).json({
+      message: "Erro Usuario não encontrado",
+    });
+  } else {
+    return response.status(200).json(Credencial);
+  }
+};
+
+//------------------------------------------------------------------
 module.exports = {
   getID,
   getAllUsuarios,
   createUsuario,
   deleteUsuario,
   updateUsuario,
+  validaUsuario,
 };
